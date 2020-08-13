a48d0615d0deb8e9d61165554e9a6aca947b27b3
Brian Gould <brian.gould@gmail.com>
2020-08-13 00:49:30 -0400

    Unity crash

---
+++ Assets/FollowPlayer.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public Transform player;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(player.position);
+    }
+}
