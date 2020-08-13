9b7fc2fb5c39757abc26f5b78d4ff7558818ebb7
Brian Gould <brian.gould@gmail.com>
2020-08-13 00:37:32 -0400

    Finished lesson 3

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,12 +4,25 @@ public class PlayerMovement : MonoBehaviour{
 
     //gives the player movement RigidBody a variable
     public Rigidbody rb;
+    
+    //allows for the forward speed to be adjusted in Unity
+    public float forwardForce = 2000f;
 
-
+    public float sidewaysForce = 500f;
     // Update is called once per frame
     //FixedUpdate is used for physics updates
     void FixedUpdate()
     {
-        rb.AddForce(0, 0, 2000 * Time.deltaTime);
+        //use Time.deltaTime to keep even force regardless of FPS
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("l"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("j"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+        }
     }
 }
