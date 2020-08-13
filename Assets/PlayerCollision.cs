9d6fbb3cdd5f5a18bae3a64a544810f1792dad6d
Brian Gould <brian.gould@gmail.com>
2020-08-13 09:14:32 -0400

    Finished lesson 5

---
+++ Assets/PlayerCollision.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+   public PlayerMovement movement;
+
+   private void OnCollisionEnter(Collision collisionInfo)
+   {
+      if (collisionInfo.collider.tag == "obstacle")
+      {
+         movement.enabled = false;
+      }
+   }
+}
